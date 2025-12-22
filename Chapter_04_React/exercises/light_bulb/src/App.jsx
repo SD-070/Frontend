dcf97623f64e503851ab5013a95df66b86f53f58
Stephan Ullmann <stephan.ullmann@posteo.de>
2025-12-22 08:47:00 +0100

    useState exercises - starter

---
+++ Chapter_04_React/exercises/light_bulb/src/App.jsx
@@ -0,0 +1,13 @@
+import LightBulb from './components/LightBulb.jsx';
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <button>Switch</button>
+      <LightBulb />
+    </>
+  );
+};
+
+export default App;
