99e44884dff967bd3c8c9b8dcae18b3c726cc9ce
Garrett Lander <garrett.jlander@gmail.com>
2026-01-21 11:12:01 +0100

    zod lecture

--- chapter07-ts-in-react/05-zod-runtime-validation-lecture/src/App.tsx
+++ chapter07-ts-in-react/05-zod-runtime-validation-lecture/src/App.tsx
@@ -1,14 +1,42 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { z } from 'zod/v4';
+
+// type Todo = {
+// 	completed: boolean;
+// 	id: number;
+// 	title: string;
+// 	userId: number;
+// };
+
+const TodoSchema = z.object({
+	completed: z.boolean(),
+	id: z.number(),
+	title: z.string('Must have a string'),
+	userId: z.number()
+});
+
+const TodoArraySchema = z.array(TodoSchema);
+
+// type Todo = z.infer<typeof TodoSchema>;
+type TodoArray = z.infer<typeof TodoArraySchema>;
 
 const App = () => {
+	const [todos, setTodos] = useState<TodoArray>([]);
 	useEffect(() => {
 		const getTodo = async () => {
 			try {
-				const res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+				const res = await fetch('https://jsonplaceholder.typicode.com/todos/');
 				if (!res.ok) throw new Error('Something went wrong!');
-				const data = await res.json();
+				const dataRes = await res.json();
+
+				const { success, data, error } = TodoArraySchema.safeParse(dataRes);
+
+				if (!success) {
+					throw new Error(z.prettifyError(error));
+				}
 
 				console.log(data);
+				setTodos(data);
 			} catch (error) {
 				console.error(error);
 			}
@@ -18,6 +46,18 @@ const App = () => {
 	return (
 		<div>
 			<h1>Runtime Validation with Zod</h1>
+			<ul>
+				{/* {todos && (
+					<li className={todos.completed ? 'line-through' : ''}>
+						{todos.title}
+					</li>
+				)} */}
+				{todos.map((todo) => (
+					<li key={todo.id} className={todo.completed ? 'line-through' : ''}>
+						{todo.title}
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 };
